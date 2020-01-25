e5bda2bed94c7291a503732967413ffc6a6e3dfb
electriclantern <quewon.chin@gmail.com>
2020-01-25 22:48:33 +0900

    beginnings of system layout

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 (work in progress js game)
 
-you are an amnesiac AI managing a broken intergalactic transportation system. all while the world is ending.
+you are an amnesiac AI managing a broken intergalactic transportation system.
---
+++ js/input.js
@@ -0,0 +1,181 @@
+var s = {
+	O: [1, 1, 1,
+		1, 0, 1,
+		1, 1, 1],
+	X: [1, 0, 1,
+		0, 1, 0,
+		1, 0, 1],
+	H: [1, 0, 1,
+		1, 1, 1,
+		1, 0, 1],
+	I: [1, 1, 1,
+		0, 1, 0,
+		1, 1, 1]
+}
+
+function input() {
+	if (document.title.toLowerCase() != "oxhi") {
+		if (isShape(s.O)) {
+			menu('O')
+		}
+
+		if (document.title == "main" || document.title == "welcome.") {
+			if (isShape(s.X)) {
+				menu('X')
+			}
+
+			else if (isShape(s.H)) {
+				menu('H');
+				//let's have an animation that looks like squiggly lines wiggling across the screen
+				//to indicate that a transmission is being attempted
+			}
+
+			else if (isShape(s.I)) {
+				menu('I')
+			}
+		}
+	}
+
+	else if (document.title.toLowerCase() == "oxhi") {
+		if (isShape(s.O)) {
+			title("O", "o")
+		}
+
+		else if (isShape(s.X)) {
+			title("X", "x");
+		}
+
+		else if (isShape(s.H)) {
+			title("H", "h");
+		}
+
+		else if (isShape(s.I)) {
+			title("I", "i");
+		}
+
+		if (document.title == "oxhi") {
+			document.title = "welcome.";
+			animation_OXHI();
+		}
+	}
+}
+
+function isShape(shape) {
+	let parsed = true;
+
+	for (let i = 0; i < 9; i++) {
+		if ((cstate[i] == false && shape[i] == 1) ||
+			(cstate[i] == true && shape[i] == false)) {
+			parsed = false
+		}
+	}
+
+	return parsed;
+}
+
+function clearGrid() {
+	for (let i = 0; i < 9; i++) {
+		if (cstate[i]) { cstate[i] = !cstate[i] }
+	}
+}
+
+function title(a, b) {
+	document.title = document.title.replace(a, b);
+	clearGrid();
+}
+function menu(letter) {
+	if (letter == 'O') {
+		document.title = "main";
+		c.color = c.color_o
+	}
+	else if (letter == 'H') {
+		document.title = "helix_watch";
+		c.color = c.color_h
+	}
+	else if (letter == 'I') {
+		document.title = "systems";
+		c.color = c.color_i
+	}
+	else if (letter == 'X') {
+		document.title = "hq";
+		c.color = c.color_x
+	}
+	clearGrid()
+}
+
+var locked = false;
+function lock(bool) { //stop user from inputting
+	if (bool == true) {
+		locked = true;
+		canvas.style.cursor = 'auto';
+	} else {
+		locked = false;
+		canvas.style.cursor = 'pointer';
+	}
+}
+
+//grid animations
+function animation_loading() {
+	lock(true);
+
+	var time = 0;
+	var int = 150;
+	var timelimit = 2000;
+
+	//every half a second, a random tile is colored 'c.color_i'
+	var prevnum = 0;
+	var num = 0;
+	var animation = setInterval(function() {
+		time += int;
+
+		prevnum = num;
+		while (num == prevnum) {
+			num = Math.floor(Math.random()*9);
+		}
+
+		if (time > timelimit) {
+			note_to_artist = false;
+			lock(false);
+			clearInterval(animation);
+			return
+		}
+
+		note_to_artist = [[num], c.color_i];
+	}, int)
+}
+
+function animation_OXHI() {
+	o_x_h_i.play();
+	lock(true);
+	bro.textContent = "";
+
+	var time = 0;
+	var int = 500;
+	var timelimit = 2500;
+
+	var animation = setInterval(function() {
+		time += int;
+
+		if (time > timelimit) {
+			note_to_artist = false;
+			lock(false);
+			clearInterval(animation);
+
+			//OXHI UNLOCKED
+			clearDialogue('bro');
+			say(dialogue_string);
+			oxhi_song.play();
+			return
+		}
+
+		if (time == 500) {
+			note_to_artist = [[0, 1, 2, 3, 5, 6, 7, 8], c.color_i];
+		} else if (time == 1000) {
+			note_to_artist = [[0, 2, 4, 6, 8], c.color_i];
+		} else if (time == 1500) {
+			note_to_artist = [[0, 2, 3, 4, 5, 6, 8], c.color_i];
+		} else if (time == 2000) {
+			note_to_artist = [[0, 1, 2, 4, 6, 7, 8], c.color_i];
+		}
+	}, int)
+}
